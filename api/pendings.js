f5e6b8ed88e0ad745918551c3015b1ab18ead464
Nahuel <nahuel.trucco.bkn@gmail.com>
2020-11-22 16:57:07 -0300

    GET by name, zone, and job for pendings: Done!

--- api/pendings.js
+++ api/pendings.js
@@ -26,6 +26,42 @@ router.post("/pendings", (req, res)=>{
     .catch((error) => { res.status(500).send({ error: "An error has ocurred." }) });
 });
 
+/* GET by name */
+router.get("/pendings/names/:name", (req, res) => {
+    const NAME_REQUIRED = {$regex:req.params.name};
+
+    Pending.find({name: NAME_REQUIRED})
+    .then((nameRequiredPendings)=>{
+        nameRequiredPendings.length === 0 && (res.status(400).send({error: `${NAME_REQUIRED} not found.`}))
+        nameRequiredPendings.length > 0 && ( res.status(200).send(nameRequiredPendings))
+    })
+    .catch( error => { res.status(500).send({ error: "An error has ocurred." })});
+});
+
+/* GET by zone */
+router.get("/pendings/zones/:zone", (req, res) => {
+    const ZONE_REQUIRED = {$regex:req.params.zone};
+
+    Pending.find({zone: ZONE_REQUIRED})
+    .then((zoneRequiredPendings)=>{
+        zoneRequiredPendings.length === 0 && (res.status(400).send({error: `${ZONE_REQUIRED} not found.`}))
+        zoneRequiredPendings.length > 0 && ( res.status(200).send(zoneRequiredPendings))
+    })
+    .catch((error) => { res.status(500).send({ error: "An error has ocurred." })});
+});
+
+/* GET by job */
+router.get("/pendings/jobs/:job", (req, res) => {
+    const JOB_REQUIRED = {$regex:req.params.job};
+
+    Pending.find({job: JOB_REQUIRED})
+    .then((jobRequiredPendings)=>{
+        jobRequiredPendings.length === 0 && (res.status(400).send({error: `${JOB_REQUIRED} not found.`}))
+        jobRequiredPendings.length > 0 && ( res.status(200).send(jobRequiredPendings))
+    })
+    .catch((error) => { res.status(500).send({ error: "An error has ocurred." })});
+});
+
 
 
 
