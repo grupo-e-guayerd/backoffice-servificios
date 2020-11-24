c170faf1bac0d258b465075c070eacf617b0d499
fedecanesa <44520607+fedecanesa@users.noreply.github.com>
2020-11-24 20:18:19 -0300

    Add mercadopago

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-
+const mercadopago = require("mercadopago")
 const URL_DB_BACKOFFICE = require("./config"); 
+const { response } = require("express");
 
 // APP 
 const app = express();
@@ -24,6 +25,31 @@ app.use(require("./api/categories"));
 app.use(require("./api/zones"));
 
 
+
+app.get("/mercadopago" , (req , res) => {
+    
+    let preference = {
+        items: [
+        {  
+            title: "Un prodructo",
+            unit_price: 1000,
+            quantity: 1
+        }
+        ]
+        /* back_urls: {
+            success: "https://servi-oficios.vercel.app/"
+        }*/
+    } 
+    
+    mercadopago.preferences.create(preference)
+    .then( response =>  {
+        return res.status(200).send({init_point:response.body.init_point})
+    })
+    .catch( error => console.log(error))
+})
+
+
+
 // DATABASE AND SERVER CONECTION
 mongoose.connect(URL_DB_BACKOFFICE, { useNewUrlParser: true, useUnifiedTopology: true }, (error) => {
     if (error) { console.log("Error on try connect database.\n") }
@@ -31,10 +57,18 @@ mongoose.connect(URL_DB_BACKOFFICE, { useNewUrlParser: true, useUnifiedTopology:
         console.log("Database conected.\n")
 
         app.listen(app.get("port"), (error) => {
-            !error ? (
+            if(!error){
                 console.log(`Server running in port ${app.get("port")}\n`)
-            )
-            : (console.log("Error at trying to run Backserver."))
+               
+                mercadopago.configure({
+                    access_token: "TEST-1523691645272520-112421-432fc7e39348bba6890a1196d55b5487-677364570"
+                })
+            } 
+            else {
+                console.log("Error at trying to run Backserver.")
+            }
+                
+            
             
         })
     }
