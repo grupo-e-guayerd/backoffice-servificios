af0de3b0bc3c969c4065891982f34055a9e81496
Matias Aquino <m4tias.a@gmail.com>
2020-11-24 21:33:51 -0300

    Admin model added

--- models/Admin.js
+++ models/Admin.js
@@ -1,8 +1,32 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const adminSchema = new mongoose.Schema({
-    admin: String,
-    password: String,
+    admin: {
+        type:String,
+        requiered:true,
+        unique:true,
+    },
+    password: {
+        type:String,
+        requiered:true
+    }
 })
+
+//HASH
+adminSchema.pre("save", function(next) {
+
+    bcrypt.genSalt(10)
+    .then(salt=> {
+        bcrypt.hash(this.password, salt)
+        .then(hash=> {
+            this.password = hash;
+            next();
+        })
+        .catch(err=> next(err));        
+    })
+    .catch(err=> next(err));
+
+});
 
 module.exports = mongoose.model("Admin", adminSchema);
