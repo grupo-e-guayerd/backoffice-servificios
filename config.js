df860b353fe928e93d48cf80ed3c57bcbd352d80
Federico Leonel Canesa <federicocanesa@gmail.com>
2021-07-26 22:04:08 -0300

    Algunos cambios

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
-const URL_DB_BACKOFFICE = "mongodb+srv://grupoe:proyectofinal@cluster0.vuk9e.mongodb.net/finalProyect?retryWrites=true&w=majority";
+const URL_DB_BACKOFFICE =
+  "mongodb+srv://grupoe:proyectofinal@cluster0.vuk9e.mongodb.net/finalProyect?retryWrites=true&w=majority";
 
 module.exports = URL_DB_BACKOFFICE;
--- index.js
+++ index.js
@@ -2,22 +2,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const mercadopago = require("mercadopago")
-const URL_DB_BACKOFFICE = require("./config"); 
+const mercadopago = require("mercadopago");
+const URL_DB_BACKOFFICE = require("./config");
 const { response } = require("express");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const Admin = require("./models/Admin.js");
 
-// APP 
+// APP
 const app = express();
 
-// MIDDLEWARES 
+// MIDDLEWARES
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// SETTINGS 
+// SETTINGS
 app.set("port", process.env.PORT || 3001);
 
 /* ENDPOINTS */
@@ -27,52 +27,47 @@ app.use(require("./api/admins"));
 app.use(require("./api/categories"));
 app.use(require("./api/zones"));
 
+app.get("/mercadopago", (req, res) => {
+  let preference = {
+    items: [
+      {
+        title: "Un prodructo",
+        unit_price: 1000,
+        quantity: 1,
+      },
+    ],
+  };
 
-
-app.get("/mercadopago" , (req , res) => {
-    
-    let preference = {
-        items: [
-        {  
-            title: "Un prodructo",
-            unit_price: 1000,
-            quantity: 1
-        }
-        ]
-        /* back_urls: {
-            success: "https://servi-oficios.vercel.app/"
-        }*/
-    } 
-    
-    mercadopago.preferences.create(preference)
-    .then( response =>  {
-        return res.status(200).send({init_point:response.body.init_point})
+  mercadopago.preferences
+    .create(preference)
+    .then((response) => {
+      return res.status(200).send({ init_point: response.body.init_point });
     })
-    .catch( error => console.log(error))
-})
-
-
+    .catch((error) => console.log(error));
+});
 
 // DATABASE AND SERVER CONECTION
-mongoose.connect(URL_DB_BACKOFFICE, { useNewUrlParser: true, useUnifiedTopology: true }, (error) => {
-    if (error) { console.log("Error on try connect database.\n") }
-    else {
-        console.log("Database conected.\n")
+mongoose.connect(
+  URL_DB_BACKOFFICE,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (error) => {
+    if (error) {
+      console.log("Error on try connect database.\n");
+    } else {
+      console.log("Database conected.\n");
 
-        app.listen(app.get("port"), (error) => {
-            if(!error){
-                console.log(`Server running in port ${app.get("port")}\n`)
-               
-                mercadopago.configure({
-                    access_token: "TEST-1523691645272520-112421-432fc7e39348bba6890a1196d55b5487-677364570"
-                })
-            } 
-            else {
-                console.log("Error at trying to run Backserver.")
-            }
-                
-            
-            
-        })
+      app.listen(app.get("port"), (error) => {
+        if (!error) {
+          console.log(`Server running in port ${app.get("port")}\n`);
+
+          mercadopago.configure({
+            access_token:
+              "TEST-3985581771884986-112217-c8858a875a1f0d2dd6aaefce5f1a0393-238730444",
+          });
+        } else {
+          console.log("Error at trying to run Backserver.");
+        }
+      });
     }
-})
+  }
+);
